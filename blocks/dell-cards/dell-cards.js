02a541e55ccb1a72569bf8fce9f07f909bcfd6e6
pranjal-yadav-qa <pranjal.yadav@axeno.co>
2026-05-18 20:00:04 +0530

    Issue1:Fixed JS linting issue

--- blocks/dell-cards/dell-cards.js
+++ blocks/dell-cards/dell-cards.js
@@ -13,52 +13,52 @@ export default function decorate(block) {
 
   // ===== EYEBROW =====
 
-  rows[0].classList.add("eyebrow");
+  rows[0].classList.add('eyebrow');
 
   // ===== HEADING =====
 
   const headingText = rows[1].textContent.trim();
 
-  const heading = document.createElement("h2");
+  const heading = document.createElement('h2');
   heading.textContent = headingText;
 
   rows[1].replaceWith(heading);
 
   // ===== CARDS WRAPPER =====
 
-  const cardsWrapper = document.createElement("div");
-  cardsWrapper.className = "cards";
+  const cardsWrapper = document.createElement('div');
+  cardsWrapper.className = 'cards';
 
   const imageCols = [...rows[2].children];
   const titleCols = [...rows[3].children];
   const linkCols = [...rows[4].children];
 
   imageCols.forEach((imgCol, index) => {
-    const card = document.createElement("article");
-    card.className = "card";
+    const card = document.createElement('article');
+    card.className = 'card';
 
     // image
 
-    const picture = imgCol.querySelector("picture");
+    const picture = imgCol.querySelector('picture');
 
     // body
 
-    const body = document.createElement("div");
-    body.className = "card-body";
+    const body = document.createElement('div');
+    body.className = 'card-body';
 
     // title
 
-    const title = document.createElement("h3");
+    const title = document.createElement('h3');
     title.textContent = titleCols[index].textContent.trim();
 
     // spacer
 
-    const spacer = document.createElement("div");
-    spacer.className = "spacer";
+    const spacer = document.createElement('div');
+    spacer.className = 'spacer';
 
     // CTA
 
-    const link = linkCols[index].querySelector("a");
+    const link = linkCols[index].querySelector('a');
 
     body.append(title);
     body.append(spacer);
@@ -88,10 +88,10 @@ export default function decorate(block) {
 
   const footerRow = rows[5];
 
-  const footerLinks = document.createElement("div");
-  footerLinks.className = "footer-links";
+  const footerLinks = document.createElement('div');
+  footerLinks.className = 'footer-links';
 
-  const footerAnchors = footerRow.querySelectorAll("a");
+  const footerAnchors = footerRow.querySelectorAll('a');
 
   footerAnchors.forEach((anchor) => {
     footerLinks.append(anchor);
